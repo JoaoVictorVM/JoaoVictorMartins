0540e50b09297c9294c735ff6328a2eeb7810822
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-04 11:55:50 -0300

    first commit

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/pages/home/hero-section";
+
+export default function Home() {
+    return (
+        <main>
+
+            <HeroSection />
+
+        </main>
+    )
+}
