104d152701b270bbf7e303490abc143339534172
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-10 14:11:52 -0300

    Refactor hero and about sections layout and styles
    Updated the layout structure and styling for the AboutSection and HeroSection components. Improved alignment, spacing, and visual hierarchy by adjusting flex containers, margins, and text styles. Added shadow effects to images for enhanced appearance.

--- src/components/pages/home/about-section.tsx
+++ src/components/pages/home/about-section.tsx
@@ -2,19 +2,19 @@ export default function AboutSection() {
     return (
         <section id="about" className="{aboutSection} h-screen bg-[var(--color-light)] flex items-center">
 
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center m-60">
 
-                <div className="w-1/2 m-60 flex flex-col gap-6">
+                <div className="w-1/2 flex flex-col">
                     <img
                         src="/assets/images/Perfil.jpg" 
                         alt="João Victor Martins"
-                        className="w-[300px] h-[300px]"
+                        className="w-[300px] h-[300px] shadow-[0_0_10px_2px_#333E50]"
                         />
                 </div>
 
-                <div>
-                    <h1>Sobre mim</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quas veritatis quisquam. Adipisci velit beatae dolor, doloribus distinctio aperiam sint in laborum perspiciatis impedit officia voluptatibus recusandae exercitationem pariatur aut?</p>
+                <div className="w-1/2">
+                    <h1 className="text-[var(--color-primary)] text-5xl font-bold">Sobre mim</h1>
+                    <p className="text-[var(--color-primary)] text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quas veritatis quisquam. Adipisci velit beatae dolor, doloribus distinctio aperiam sint in laborum perspiciatis impedit officia voluptatibus recusandae exercitationem pariatur aut?</p>
                 </div>
 
             </div>
--- src/components/pages/home/hero-section.tsx
+++ src/components/pages/home/hero-section.tsx
@@ -2,22 +2,28 @@ export default function HeroSection() {
     return (
         <section id="hero" className="h-screen bg-[var(--color-primary)] flex items-center">
 
-            <div className="w-1/2 m-60 flex flex-col gap-6">
-                <h1 className="text-[var(--color-highlight)] text-7xl font-bold">Fullstack<br/>Developer<span className="text-[var(--color-light)]">.</span></h1>
-                <p className="text-[var(--color-light)] text-2xl">I like to craft solid and scalable frontend products with great user experiences.</p>
-                <div className="flex">
-                    <p className="text-[var(--color-highlight)] mt-18 mr-25">Proven experience building successful products for clients across several countries.</p>
-                    <p className="text-[var(--color-highlight)] mt-18 mr-25">Proven experience building successful products for clients across several countries.</p>
+            <div className="flex items-center justify-center m-60 gap-60">
+
+                <div className="w-3/5 flex flex-col gap-6">
+                    <h1 className="text-[var(--color-highlight)] text-7xl font-bold">Fullstack<br/>Developer<span className="text-[var(--color-light)]">.</span></h1>
+                    <p className="text-[var(--color-light)] text-2xl">I like to craft solid and scalable frontend products with great user experiences.</p>
+                    <div className="flex">
+                        <p className="text-[var(--color-highlight)] mt-18 mr-25">Proven experience building successful products for clients across several countries.</p>
+                        <p className="text-[var(--color-highlight)] mt-18 mr-25">Proven experience building successful products for clients across several countries.</p>
+                    </div>
                 </div>
+
+                <div className="w-2/5">
+                    <img
+                        src="/assets/svg/hero-section-animate.svg"
+                        alt="Hero Image"
+                        className="w-[500px] h-[500px] shadow-"
+                    />
+                </div>
+
             </div>
 
-            <div className="w-1/2">
-                <img
-                    src="/assets/svg/hero-section-animate.svg"
-                    alt="Hero Image"
-                    className="w-[500px] h-[500px]"
-                />
-            </div>
+
 
         </section>
     )
