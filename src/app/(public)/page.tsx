2d0b0531b20b2614fef8523de3ef209e83b272c1
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-13 20:55:43 -0300

    Fiz images
    Altera o src das imagens para tirar o problema de request
    
    Exibe novamente o footer para comecar o desenvolvimento

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             <AboutSection />
             <MoreSection />
             <ProjectsSection />
-
+            <FooterSection />
 
         </main>
     )
